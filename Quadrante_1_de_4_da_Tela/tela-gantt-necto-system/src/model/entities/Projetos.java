789289b1d4f39e8d802753478f99bd6308df5e66
Caroline Sousa <61089745+caroolps@users.noreply.github.com>
2020-05-28 20:38:29 -0300

    Entrega 03 - Quadrante 1 de 4 da Tela (GANTT-NECTO)

---
+++ Quadrante_1_de_4_da_Tela/tela-gantt-necto-system/src/model/entities/Projetos.java
@@ -0,0 +1,101 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Projetos implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String taskname;
+	private String starttime;
+	private String owner;
+	private String duration;
+	
+	public Projetos() {
+	}
+
+	public Projetos(Integer id, String taskname, String starttime, String owner, String duration ) {
+		this.id = id;
+		this.taskname = taskname;
+		this.starttime = starttime;
+		this.owner = owner;
+		this.duration = duration;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTaskname() {
+		return taskname;
+	}
+
+	public void setTaskname(String taskname) {
+		this.taskname = taskname;
+	}
+
+	public String getStarttime() {
+		return starttime;
+	}
+
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+	
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	
+	
+	
+	
+	
+		
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Projetos other = (Projetos) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Projetos [id=" + id + ", taskname=" + taskname + ", starttime=" + starttime + ", owner=" + owner +", duration=" + duration + "]";
+	}
+}
