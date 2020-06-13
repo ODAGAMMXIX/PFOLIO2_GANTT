34360137da488db667199a4696cabd07dcdcaabb
Caroline Sousa <61089745+caroolps@users.noreply.github.com>
2020-06-13 15:44:42 -0300

    Entrega 04 - Tela de cadastramento com integração ao banco de dados
    Entrega 04 - Tela de cadastramento com integração ao banco de dados

---
+++ Quadrante_3_de_4_da_Tela_COMPLETO/src/model/dao/ProjetosDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Projetos;
+
+public interface ProjetosDao {
+
+	void insert(Projetos obj);
+	void update(Projetos obj);
+	void deleteById(Integer id);
+	Projetos findById(Integer id);
+	List<Projetos>findAll();
+}	
